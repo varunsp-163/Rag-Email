3ad30ff8846c7e40e39af9ace630a6e8079477ac
varunsp-163 <spvarun47@gmail.com>
2024-10-27 17:05:36 +0530

    Testing deployment

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,29 @@
+// /api/clerk/weebhook
+
+import { db } from "@/server/db";
+
+
+
+export const POST = async (req: Request) => {
+    const { data } = await req.json()
+
+    const emailAddress = data.email_addresses[0].email_address
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url
+    const id = data.id
+
+    await db.user.create({
+        data: {
+            id: id,
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: imageUrl
+        }
+
+    })
+    console.log("User was created")
+    return new Response('WebHook Received', { status: 200 })
+
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']) // by default it redirects here if the route is not protected
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/clerk/webhook(.*)']) // by default it redirects here if the route is not protected
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
